fe886089737453da0b22c1d35e7d8c5af73e97ba
Brenno Eduardo <brennobesc@gmail.com>
2022-01-22 02:13:23 -0300

    Commit

---
+++ script2.js
@@ -0,0 +1,25 @@
+function zeroFill(n) {
+    return n < 9 ? `0${n}` : `${n}`;
+  }
+  
+function formatDate(date) {
+    const dia = zeroFill(date.getDate());
+    const mes = zeroFill(date.getMonth()+ 1);
+    const ano = zeroFill(date.getFullYear())  
+
+    return `${dia}/${mes}/${ano}`
+}
+
+function render(data){
+    const date = document.querySelector('.date')
+    date.innerHTML = data;
+}
+
+var soma = Number(window.prompt('Digite um número: '))
+
+const data = new Date();
+data.setDate(data.getDate() + soma);
+data.setMonth(data.getMonth() + 0);
+data.setFullYear(data.getFullYear() + 0);
+
+render(formatDate(data));
